4b9358716ec08e54a8d7b75115f6204c40756cf9
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-03 18:07:21 -0500

    Add access token to Prismic configuration

--- src/lib/integrations/Prismic/prismic.config.ts
+++ src/lib/integrations/Prismic/prismic.config.ts
@@ -1,7 +1,8 @@
 import * as prismic from '@prismicio/client'
 
 const prismicProjectName = 'startup-template'
-const accessToken = undefined
+const accessToken =
+  'MC5aMC1QRFJNQUFDSUE5UEd3.Vu-_ve-_ve-_vTnvv71c77-977-9SQ7vv71e77-9DRNpSHjvv70ZQhYcO--_ve-_vXLvv73vv73vv71F'
 const apiEndpoint = `https://${prismicProjectName}.prismic.io/api/v2`
 const repositoryName = prismic.getRepositoryName(apiEndpoint)
 
