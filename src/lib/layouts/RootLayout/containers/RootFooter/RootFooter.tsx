acc1c6fd5ea3adbf26f0958d3c93d4e6eca93cce
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-04 14:05:39 -0500

    Refactor RootFooter layout for improved responsiveness and styling

--- src/lib/layouts/RootLayout/containers/RootFooter/RootFooter.tsx
+++ src/lib/layouts/RootLayout/containers/RootFooter/RootFooter.tsx
@@ -16,9 +16,20 @@ export default function RootFooter() {
       sx={{
         backgroundColor: 'neutral.600',
         mt: '50px',
+        display: 'grid',
+        gap: 2,
+        p: '16px',
       }}
     >
-      <Box data-id='RootFooterInner' sx={{}}>
+      <Box
+        data-id='RootFooterInner'
+        sx={{
+          margin: '0 auto',
+          width: ['100%', 'fit-content'],
+          display: 'grid',
+          gap: 4,
+        }}
+      >
         <Box data-id='RootFooterLinks' sx={{}}>
           RootFooterLinks
         </Box>
@@ -27,33 +38,65 @@ export default function RootFooter() {
           data-id='RootFooterBottom'
           sx={{
             display: 'grid',
+            width: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            gridAutoFlow: ['row', 'column'],
+            alignItems: 'center',
+            justifyContent: [, 'space-between'],
+            gap: 2,
           }}
         >
-          <FullLogoLink sx={{}} />
-          <Text
-            text={`Startup LLC. ${new Date().getFullYear()}`}
-            sx={{
-              color: 'neutral.100',
-              fontWeight: 400,
-              fontSize: 12,
-            }}
-          />
           <Box
-            data-id='RootFooterSocialLinks'
+            data-id='RootFooterBottomLeft'
             sx={{
-              display: 'flex',
+              display: 'grid',
+              justifyContent: 'space-between',
+              gridColumn: [, '1'],
+              alignContent: 'center',
+              gridAutoFlow: 'column',
+              alignItems: 'center',
+              width: '100%',
               gap: 2,
             }}
           >
-            {mainNavLinks.socialLinks.map((link) => (
-              <Link key={link.url} href={link.url} sx={{}}>
-                <link.icon
-                  sx={{
-                    width: '18px',
-                  }}
-                />
-              </Link>
-            ))}
+            <FullLogoLink
+              sx={{
+                transform: 'translateY(-2px)',
+              }}
+            />
+            <Text
+              text={`Startup LLC. ${new Date().getFullYear()}`}
+              sx={{
+                color: 'neutral.100',
+                fontWeight: 400,
+                fontSize: 12,
+              }}
+            />
+          </Box>
+
+          <Box
+            data-id='RootFooterBottomRight'
+            sx={{
+              order: ['-1', 'initial'],
+            }}
+          >
+            <Box
+              data-id='RootFooterSocialLinks'
+              sx={{
+                display: 'flex',
+                gap: 2,
+              }}
+            >
+              {mainNavLinks.socialLinks.map((link) => (
+                <Link key={link.url} href={link.url} sx={{}}>
+                  <link.icon
+                    sx={{
+                      width: '21px',
+                    }}
+                  />
+                </Link>
+              ))}
+            </Box>
           </Box>
         </Box>
       </Box>
