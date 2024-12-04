d70d9c925b93e98b8ae45bd6eb4a04f03c8fd800
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-03 19:19:14 -0500

    Refactor Prismic types and components for consistency and readability

--- src/lib/integrations/Prismic/slices/StandardPageContent/index.tsx
+++ src/lib/integrations/Prismic/slices/StandardPageContent/index.tsx
@@ -1,23 +1,21 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import React from 'react'
+import type { Content } from '@prismicio/client'
+import type { SliceComponentProps } from '@prismicio/react'
 
 /**
  * Props for `RootPage`.
  */
-export type RootPageProps = SliceComponentProps<Content.RootPageSlice>;
+export type RootPageProps = SliceComponentProps<Content.RootPageSlice>
 
 /**
  * Component for "RootPage" Slices.
  */
-const RootPage = ({ slice }: RootPageProps): JSX.Element => {
+const RootPage = ({ slice }: RootPageProps) => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
+    <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
       Placeholder component for root_page (variation: {slice.variation}) Slices
     </section>
-  );
-};
+  )
+}
 
-export default RootPage;
+export default RootPage
