4fa79da38edc4606cbf3198d995e3fb831102511
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-04 20:47:18 -0500

    Integrate Prismic data fetching and rendering in Privacy Policy page

--- src/pages/privacy-policy/index.tsx
+++ src/pages/privacy-policy/index.tsx
@@ -1,14 +1,33 @@
 import React from 'react'
+import type { InferGetStaticPropsType } from 'next'
 
-import PrivacyPolicyPage from '@/src/pagesContent/PrivacyPolicy/pages/PrivacyPolicyPage.js'
+import PrivacyPolicyPage from '../../pagesContent/PrivacyPolicy/pages/PrivacyPolicyPage.js'
+import getStandardPageByUid from '../../lib/integrations/Prismic/pageTypes/standard/getStandardPageByUid/getStandardPageByUid.js'
+import PrismicStandardPageProvider from '../../lib/integrations/Prismic/pageTypes/standard/PrismicStandardPageProvider/PrismicStandardPageProvider.js'
+
 import RootLayout from '@/src/lib/layouts/RootLayout/RootLayout.js'
 
-export default function PrivacyPolicyPageRoot() {
+export const getStaticProps = async ({ previewData }) => {
+  const res = await getStandardPageByUid({
+    previewData,
+    uid: 'privacy-policy',
+  })
+
+  return {
+    props: {
+      tosData: res,
+    },
+  }
+}
+
+export default function PrivacyPolicyPageRoot(
+  props: InferGetStaticPropsType<typeof getStaticProps>,
+) {
   return (
-    <>
-      <RootLayout title='Privacy-Policy'>
+    <PrismicStandardPageProvider getStandardPageByUidReturn={props.tosData}>
+      <RootLayout title='Privacy Policy'>
         <PrivacyPolicyPage />
       </RootLayout>
-    </>
+    </PrismicStandardPageProvider>
   )
 }
--- src/pagesContent/PrivacyPolicy/pages/PrivacyPolicyPage.tsx
+++ src/pagesContent/PrivacyPolicy/pages/PrivacyPolicyPage.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Box from '@useweb/ui/Box'
 
 import PrivacyPolicyPageLayout from '../layouts/PrivacyPolicyPageLayout/PrivacyPolicyPageLayout.js'
+import { usePrismicStandardPage } from '../../../lib/integrations/Prismic/pageTypes/standard/PrismicStandardPageProvider/PrismicStandardPageProvider.js'
+import PrismicSlicesRenderer from '../../../lib/integrations/Prismic/ui/PrismicSlicesRenderer/PrismicSlicesRenderer.js'
 
 export default function PrivacyPolicyPage() {
+  const pageData = usePrismicStandardPage()
+
   return (
     <PrivacyPolicyPageLayout>
       <Box data-id='PrivacyPolicyPage' sx={{}}>
-        PrivacyPolicy
+        <PrismicSlicesRenderer slices={pageData.data.slices} />
       </Box>
     </PrivacyPolicyPageLayout>
   )
