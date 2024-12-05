1a822e6ecd7bf0793912e5adf138e9f127ab4748
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-05 16:07:59 -0500

    Add conditional rendering to UserAccessCardSignUpErrorMessage for specific error handling

--- src/data/users/ui/UserAccessCard/containers/UserAccessCardSignUp/containers/UserAccessCardSignUpErrorMessage/UserAccessCardSignUpErrorMessage.tsx
+++ src/data/users/ui/UserAccessCard/containers/UserAccessCardSignUp/containers/UserAccessCardSignUpErrorMessage/UserAccessCardSignUpErrorMessage.tsx
@@ -10,6 +10,12 @@ export type UserAccessCardSignUpErrorMessageProps = {
 export default function UserAccessCardSignUpErrorMessage(
   props: UserAccessCardSignUpErrorMessageProps,
 ) {
+  const error = props.auth.signUp?.error
+
+  if (String(error)?.includes('(auth/popup-closed-by-user)')) {
+    return null
+  }
+
   return (
     <ErrorMessage error={props.auth.signUp?.error} message={'Error creating account'} />
   )
