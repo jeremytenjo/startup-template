16a035ae6026511967c9ab1f7b3b2088c7843c7e
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-05 15:50:24 -0500

    Refactor UserAccessCardSignUp to simplify component structure and integrate last step for email sign-up

--- src/data/users/ui/UserAccessCard/containers/UserAccessCardSignUp/UserAccessCardSignUp.tsx
+++ src/data/users/ui/UserAccessCard/containers/UserAccessCardSignUp/UserAccessCardSignUp.tsx
@@ -1,23 +1,15 @@
 import React from 'react'
-import Box from '@useweb/ui/Box'
-import Text from '@useweb/ui/Text'
-import Form, { useFormContext } from '@useweb/ui/Form'
-import ErrorMessage from '@useweb/ui/ErrorMessage'
-import TextField from '@useweb/ui/TextField'
+import Form from '@useweb/ui/Form'
 
-import type { SignUpFetcherProps } from '../../../../utils/useAuth/useAuth.js'
 import useAuth from '../../../../utils/useAuth/useAuth.js'
-import { validatePassword } from '../../../../utils/signUp/signUpFormUtils/signUpFormUtils.js'
-import AccountAccessCta from '../../../AccountAccessCTA/AccountAccessCta.js'
-import postHogEventClick from '../../../../../../lib/integrations/PostHog/events/browser/postHogEventClick/postHogEventClick.js'
-import ContinueWithGoogleButton from '../../../../utils/signIn/ContinueWithGoogle/ui/ContinueWithGoogleButton/ContinueWithGoogleButton.js'
 import useOnSignUpActions from '../../../../utils/signUp/useOnSignUpActions/useOnSignUpActions.js'
 
 import UserAccessCardSignUpExistingUser from './containers/UserAccessCardSignUpExistingUser/UserAccessCardSignUpExistingUser.js'
+import UserAccessCardSignUpLastStep from './containers/UserAccessCardSignUpLastStep/UserAccessCardSignUpLastStep.js'
 
 export type UserAccessCardSignUpProps = any
 
-type UserAccessCardSignUpFormSchema = {
+export type UserAccessCardSignUpFormSchema = {
   email: string
   password: string
   username: string
@@ -26,7 +18,6 @@ type UserAccessCardSignUpFormSchema = {
 }
 
 export default function UserAccessCardSignUp() {
-  const [showEmailSignUpoptions, setShowEmailSignUpoptions] = React.useState(false)
   const onSignUpAction = useOnSignUpActions()
   const auth = useAuth({
     onSignUp: () => {
@@ -50,124 +41,8 @@ export default function UserAccessCardSignUp() {
         })
       }}
     >
-      {auth.user?.id ? (
-        <UserAccessCardSignUpExistingUser />
-      ) : (
-        <Box
-          data-id='UserAccessCardSignUpNewAccount'
-          sx={{
-            display: 'grid',
-            gap: 2,
-          }}
-        >
-          <UserAccessCardSignUpFormContinueWithGoogle />
-
-          <Text
-            text={`Or sign up via email`}
-            sx={{
-              textAlign: 'center',
-            }}
-          />
-
-          {showEmailSignUpoptions ? (
-            <UserAccessCardSignUpWithEmailFormFields />
-          ) : (
-            <AccountAccessCta
-              loading={auth.signUp.loading}
-              text='Continue with Email'
-              buttonProps={{
-                type: 'button',
-                onClick: () => setShowEmailSignUpoptions(true),
-              }}
-            />
-          )}
-
-          <ErrorMessage error={auth.signUp?.error} message={'Error creating account'} />
-        </Box>
-      )}
+      {auth.user?.id && <UserAccessCardSignUpExistingUser />}
+      {!auth.user?.id && <UserAccessCardSignUpLastStep auth={auth} />}
     </Form>
-  )
-}
-
-const UserAccessCardSignUpFormContinueWithGoogle = () => {
-  const auth = useAuth()
-  const formContext = useFormContext<UserAccessCardSignUpFormSchema>()
-
-  const signUpWithGoogleProps: SignUpFetcherProps['signUpWithGoogle'] = {
-    photoUrl: formContext.watch('photoUrl'),
-    bannerUrl: formContext.watch('bannerUrl'),
-  }
-
-  const onContinueWithGoogleClick = () => {
-    postHogEventClick({
-      action: 'user sign up: Continue with Google',
-    })
-
-    auth.signUp.exec({ signUpWithGoogle: signUpWithGoogleProps })
-  }
-
-  return <ContinueWithGoogleButton onClick={onContinueWithGoogleClick} />
-}
-
-const UserAccessCardSignUpWithEmailFormFields = () => {
-  const auth = useAuth()
-
-  return (
-    <>
-      <TextField<UserAccessCardSignUpFormSchema>
-        name='email'
-        type='email'
-        placeholder='Email'
-        label='E-mail'
-        required='Missing email'
-        inputProps={{
-          autoCapitalize: 'off',
-          autoCorrect: 'off',
-        }}
-        sx={{
-          width: '100%',
-        }}
-        validate={(value) => {
-          if (value.includes('socialseed') && value !== 'cole@socialseed.com') {
-            return 'Invalid email'
-          }
-        }}
-      />
-
-      <TextField<UserAccessCardSignUpFormSchema>
-        id='new-password'
-        type='password'
-        name='password'
-        label={'Password'}
-        placeholder='Password'
-        required='Missing password'
-        inputProps={{
-          autoComplete: 'new-password',
-          autoCapitalize: 'off',
-        }}
-        registerProps={{
-          validate: (value) => {
-            const res = validatePassword(value)
-            return res.isValid ? true : res.message
-          },
-        }}
-        sx={{
-          width: '100%',
-        }}
-      />
-
-      <AccountAccessCta
-        loading={auth.signUp.loading}
-        text='Continue with Email'
-        buttonProps={{
-          variant: 'text',
-          onClick: () => {
-            postHogEventClick({
-              action: 'user sign up: Continue with Email',
-            })
-          },
-        }}
-      />
-    </>
   )
 }
---
+++ src/data/users/ui/UserAccessCard/containers/UserAccessCardSignUp/containers/UserAccessCardSignUpLastStep/UserAccessCardSignUpLastStep.tsx
@@ -0,0 +1,127 @@
+import React from 'react'
+import Box from '@useweb/ui/Box'
+import { useFormContext } from '@useweb/ui/Form'
+import TextField from '@useweb/ui/TextField'
+import Text from '@useweb/ui/Text'
+import ErrorMessage from '@useweb/ui/ErrorMessage'
+
+import type {
+  SignUpFetcherProps,
+  UseAuthReturn,
+} from '../../../../../../utils/useAuth/useAuth.js'
+import type { UserAccessCardSignUpFormSchema } from '../../UserAccessCardSignUp.js'
+import postHogEventClick from '../../../../../../../../lib/integrations/PostHog/events/browser/postHogEventClick/postHogEventClick.js'
+import ContinueWithGoogleButton from '../../../../../../utils/signIn/ContinueWithGoogle/ui/ContinueWithGoogleButton/ContinueWithGoogleButton.js'
+import AccountAccessCta from '../../../../../AccountAccessCTA/AccountAccessCta.js'
+import { validatePassword } from '../../../../../../utils/signUp/signUpFormUtils/signUpFormUtils.js'
+
+export type UserAccessCardSignUpLastStepProps = { auth: UseAuthReturn }
+
+export default function UserAccessCardSignUpLastStep(
+  props: UserAccessCardSignUpLastStepProps,
+) {
+  const [showEmailSignUpoptions, setShowEmailSignUpoptions] = React.useState(false)
+  const formContext = useFormContext<UserAccessCardSignUpFormSchema>()
+
+  const signUpWithGoogleProps: SignUpFetcherProps['signUpWithGoogle'] = {
+    photoUrl: formContext.watch('photoUrl'),
+    bannerUrl: formContext.watch('bannerUrl'),
+  }
+
+  return (
+    <Box
+      data-id='UserAccessCardSignUpLastStep'
+      sx={{
+        display: 'grid',
+        gap: 2,
+      }}
+    >
+      <ContinueWithGoogleButton
+        onClick={() => {
+          postHogEventClick({
+            action: 'user sign up: Continue with Google',
+          })
+
+          props.auth.signUp.exec({ signUpWithGoogle: signUpWithGoogleProps })
+        }}
+      />
+
+      <Text
+        text={`Or sign up via email`}
+        sx={{
+          textAlign: 'center',
+        }}
+      />
+
+      {showEmailSignUpoptions ? (
+        <>
+          <TextField<UserAccessCardSignUpFormSchema>
+            name='email'
+            type='email'
+            placeholder='Email'
+            label='E-mail'
+            required='Missing email'
+            inputProps={{
+              autoCapitalize: 'off',
+              autoCorrect: 'off',
+            }}
+            sx={{
+              width: '100%',
+            }}
+            validate={(value) => {
+              if (value.includes('socialseed') && value !== 'cole@socialseed.com') {
+                return 'Invalid email'
+              }
+            }}
+          />
+
+          <TextField<UserAccessCardSignUpFormSchema>
+            id='new-password'
+            type='password'
+            name='password'
+            label={'Password'}
+            placeholder='Password'
+            required='Missing password'
+            inputProps={{
+              autoComplete: 'new-password',
+              autoCapitalize: 'off',
+            }}
+            registerProps={{
+              validate: (value) => {
+                const res = validatePassword(value)
+                return res.isValid ? true : res.message
+              },
+            }}
+            sx={{
+              width: '100%',
+            }}
+          />
+
+          <AccountAccessCta
+            loading={props.auth.signUp.loading}
+            text='Continue with Email'
+            buttonProps={{
+              variant: 'text',
+              onClick: () => {
+                postHogEventClick({
+                  action: 'user sign up: Continue with Email',
+                })
+              },
+            }}
+          />
+        </>
+      ) : (
+        <AccountAccessCta
+          loading={props.auth.signUp.loading}
+          text='Continue with Email'
+          buttonProps={{
+            type: 'button',
+            onClick: () => setShowEmailSignUpoptions(true),
+          }}
+        />
+      )}
+
+      <ErrorMessage error={props.auth.signUp?.error} message={'Error creating account'} />
+    </Box>
+  )
+}
