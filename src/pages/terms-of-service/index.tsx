926c4de965c6ab6b9632ac98d95b556e7eca1388
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-04 19:35:52 -0500

    Add getStandardPageByUid function and implement static props for Terms of Service page

--- src/pages/terms-of-service/index.tsx
+++ src/pages/terms-of-service/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import type { GetStaticProps } from 'next'
 
 import TermsOfServicePage from '../../pagesContent/TermsOfService/pages/TermsOfServicePage.js'
+import getStandardPageByUid from '../../lib/integrations/Prismic/pageTypes/standard/getStandardPageByUid/getStandardPageByUid.js'
 
 import RootLayout from '@/src/lib/layouts/RootLayout/RootLayout.js'
+
+export const getStaticProps = (async ({ previewData }) => {
+  const res = await getStandardPageByUid({
+    previewData,
+    uid: 'terms-of-service',
+  })
+
+  return {
+    props: {
+      faqPPage: res,
+    },
+  }
+}) satisfies GetStaticProps<any>
 
 export default function TermsOfServicePageRoot() {
   return (
