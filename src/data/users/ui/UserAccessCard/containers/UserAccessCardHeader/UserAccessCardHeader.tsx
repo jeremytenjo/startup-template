38de8ef1e21f805eb2d20cea27fb6afd2c1c9a3c
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-05 09:20:00 -0500

    Enhance UserAccessCardHeader by adding a message prop and replacing LogoIcon with an Image component for improved logo display

--- src/data/users/ui/UserAccessCard/containers/UserAccessCardHeader/UserAccessCardHeader.tsx
+++ src/data/users/ui/UserAccessCard/containers/UserAccessCardHeader/UserAccessCardHeader.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Box from '@useweb/ui/Box'
 import Text from '@useweb/ui/Text'
+import Image from '@useweb/ui/Image'
 
-import LogoIcon from '../../../../../../lib/components/icons/LogoIcon.js'
 import appConfig from '../../../../../../../app.config.js'
 import { LinkTabsList } from '../../../../../../lib/components/navigation/LinkTabs/LinkTabs.js'
 import { allNavLinks } from '../../../../../mainNavLinks/utils/useMainNavLinks/useMainNavLinks.js'
@@ -10,6 +10,7 @@ import { allNavLinks } from '../../../../../mainNavLinks/utils/useMainNavLinks/u
 export type UserAccessCardHeaderProps = {
   title?: string
   subTitle?: string
+  message?: string
 }
 
 export default function UserAccessCardHeader(props: UserAccessCardHeaderProps) {
@@ -17,15 +18,70 @@ export default function UserAccessCardHeader(props: UserAccessCardHeaderProps) {
   const subTitle = props.subTitle || `Welcome! Please enter your details`
 
   return (
-    <Box data-id='UserAccessCardHeader' sx={{}}>
-      <Box data-id='UserAccessCardTop' sx={{}}>
-        <LogoIcon
+    <Box
+      data-id='UserAccessCardHeader'
+      sx={{
+        mb: '50px',
+      }}
+    >
+      <Box
+        data-id='UserAccessCardTop'
+        sx={{
+          display: 'grid',
+          justifyItems: 'center',
+          gap: 2,
+          mb: '50px',
+        }}
+      >
+        <Image
+          src={`/images/logo/logo.svg`}
+          alt={`alt`}
+          width={300}
+          height={300}
           sx={{
-            width: '50px',
+            width: ['40px', '50px'],
+            height: ['40px', '50px'],
+            mb: [1, 2],
           }}
         />
-        <Text text={title} tag='p' sx={{}} />
-        <Text text={subTitle} tag='p' sx={{}} />
+
+        <Text
+          text={title}
+          tag='p'
+          sx={{
+            color: 'neutral.100',
+            fontWeight: 600,
+            fontSize: 23,
+            lineHeight: '32px',
+            textAlign: 'center',
+            justifySelf: 'center',
+          }}
+        />
+
+        <Text
+          text={subTitle}
+          tag='p'
+          sx={{
+            color: 'neutral.200',
+            fontWeight: 500,
+            fontSize: '14px',
+            textAlign: 'center',
+            mt: '-5px',
+          }}
+        />
+
+        {props.message && (
+          <Text
+            text={props.message}
+            sx={{
+              color: 'neutral.100',
+              fontWeight: 400,
+              fontSize: 14,
+              textAlign: 'left',
+              mt: 2,
+            }}
+          />
+        )}
       </Box>
 
       <LinkTabsList
