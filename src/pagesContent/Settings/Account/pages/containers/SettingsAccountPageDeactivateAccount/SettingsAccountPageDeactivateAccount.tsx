93feb764b60adf16ddbb9a21b3ee53a19cda431d
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-07 21:12:26 -0500

    Update SettingsAccountPageDeactivateAccount.tsx
    Fixed single single CTA prop

--- src/pagesContent/Settings/Account/pages/containers/SettingsAccountPageDeactivateAccount/SettingsAccountPageDeactivateAccount.tsx
+++ src/pagesContent/Settings/Account/pages/containers/SettingsAccountPageDeactivateAccount/SettingsAccountPageDeactivateAccount.tsx
@@ -56,7 +56,7 @@ export default function SettingsAccountPageDeactivateAccount() {
                 })
               },
             }}
-            singleCta
+            singleCTA
             customErrorComponent={(p) => {
               let errorMessage = 'Error deactivating account. Please try again.'
 
