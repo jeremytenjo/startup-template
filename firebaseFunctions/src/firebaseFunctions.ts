38947e11506d023b2f549b4ad45578f64559fbb5
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-09 18:02:33 -0500

    Add stripe webhooks receiver function to Firebase; implement onRequest handler

--- firebaseFunctions/src/firebaseFunctions.ts
+++ firebaseFunctions/src/firebaseFunctions.ts
@@ -1,6 +1,7 @@
-import { onCall } from 'firebase-functions/v2/https'
+import { onCall, onRequest } from 'firebase-functions/v2/https'
 
 import miscFunctionsFirebase from './miscFunctions/miscFunctions.firebase.js'
+import stripeWebhooksReceiverFirebase from './stripeWebhooksReceiver/stripeWebhooksReceiver.firebase.js'
 
 // miscFunctions
 export const miscFunctions = onCall(
@@ -8,4 +9,9 @@ export const miscFunctions = onCall(
     memory: '1GiB',
   },
   async (request) => await miscFunctionsFirebase({ request }),
+)
+
+// stripeWebhooksReceiver
+export const stripeWebhooksReceiver = onRequest(
+  async (req, res) => await stripeWebhooksReceiverFirebase({ req, res }),
 )
