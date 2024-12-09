dbfe74345149607f414aa76cdf4d7e03b35b20c6
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-08 21:11:35 -0500

    Enhance BillingStripeAccountPage by integrating user authentication and updating CreateConnectedAccountCard with user-specific props

--- src/pagesContent/Settings/Billing/pages/pages/BillingStripeAccount/pages/BillingStripeAccountPage.tsx
+++ src/pagesContent/Settings/Billing/pages/pages/BillingStripeAccount/pages/BillingStripeAccountPage.tsx
@@ -3,12 +3,18 @@ import Box from '@useweb/ui/Box'
 
 import BillingStripeAccountPageLayout from '../layouts/BillingStripeAccountPageLayout/BillingStripeAccountPageLayout.js'
 import CreateConnectedAccountCard from '../../../../../../../../firebaseFunctions/src/miscFunctions/routes/stripe/createConnectedAccount/ui/CreateConnectedAccountCard/CreateConnectedAccountCard.js'
+import useAuth from '../../../../../../../data/users/utils/useAuth/useAuth.js'
 
 export default function BillingStripeAccountPage() {
+  const auth = useAuth()
+
   return (
     <BillingStripeAccountPageLayout>
       <Box data-id='BillingStripeAccountPage' sx={{}}>
-        <CreateConnectedAccountCard />
+        <CreateConnectedAccountCard
+          createStripeAccountSubTitle='Create a Stripe account to receive payments.'
+          userToCreateAccount={auth.user}
+        />
       </Box>
     </BillingStripeAccountPageLayout>
   )
