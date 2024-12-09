974f9470c360facac8c2a5fb171a197dcbed8fe2
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-09 17:13:08 -0500

    Refactor useCreateStripeConnectedAccount to utilize useMiscFunctionsClient; streamline account creation logic and error handling

--- firebaseFunctions/src/miscFunctions/routes/stripe/createConnectedAccount/useCreateStripeConnectedAccount/useCreateStripeConnectedAccount.ts
+++ firebaseFunctions/src/miscFunctions/routes/stripe/createConnectedAccount/useCreateStripeConnectedAccount/useCreateStripeConnectedAccount.ts
@@ -2,70 +2,41 @@ import useAsync from '@useweb/use-async'
 import { refetchFirestoreUser } from '@useweb/firebase/useFirebaseAuth'
 
 import type UserSchema from '../../../../../../../src/data/users/user.schema.js'
-import useAuth from '../../../../../../../src/data/users/utils/useAuth/useAuth.js'
-import logError from '../../../../../../../src/lib/utils/loggers/logError/logError.js'
-import miscFunctionsClient from '../../../../miscFunctions.client.js'
-import type {
-  API_CreateConnectedAccountProps,
-  CreateConnectedAccountReturn,
-} from '../createConnectedAccount.js'
+import type { API_CreateConnectedAccountProps } from '../createConnectedAccount.js'
 import useStripeUtilsConfig from '../../../../../../../src/lib/integrations/Stripe/utils/useStripeUtilsConfig/useStripeUtilsConfig'
+import { useMiscFunctionsClient } from '../../../../utils/useMiscFunctionsClient/useMiscFunctionsClient'
 
 export type UseCreateConnectedAccountProps = { userToCreateAccount: UserSchema }
-
-export async function createStripeConnectedAccount(
-  props: UseCreateConnectedAccountProps,
-) {
-  const stripeConfig = useStripeUtilsConfig()
-
-  const res = await miscFunctionsClient<API_CreateConnectedAccountProps>({
-    api: {
-      route: 'routes/createConnectedAccount',
-      payload: {
-        userToCreateAccount: props.userToCreateAccount,
-        refreshUrl: stripeConfig.refreshUrl,
-        returnUrl: stripeConfig.returnUrl,
-      },
-    },
-  })
-
-  if (!res.data?.[0]?.createdConnectedAccount?.id) {
-    throw new Error('Failed to create connected account')
-  }
-
-  await refetchFirestoreUser()
-
-  window.location.href = res.data?.[0].accountLink.url
-
-  return res.data
-}
 
 export default function useCreateStripeConnectedAccount(
   props: UseCreateConnectedAccountProps,
 ) {
-  const auth = useAuth()
+  const stripeConfig = useStripeUtilsConfig()
 
-  const _createConnectedAccount = useAsync<
-    UseCreateConnectedAccountProps,
-    Awaited<CreateConnectedAccountReturn>['data']
-  >({
-    fn: async (execProps = props) => {
-      if (!auth.user?.id) {
-        throw new Error('User is not logged in')
-      }
+  const createStripeConnectedAccount =
+    useMiscFunctionsClient<API_CreateConnectedAccountProps>({
+      api: {
+        route: 'routes/createConnectedAccount',
+        payload: {
+          refreshUrl: stripeConfig.refreshUrl,
+          returnUrl: stripeConfig.returnUrl,
+          userToCreateAccount: props.userToCreateAccount,
+        },
+      },
+      options: {
+        onResult: async (p) => {
+          if (!p.result.data?.[0]?.createdConnectedAccount?.id) {
+            throw new Error('Failed to create connected account')
+          }
 
-      return createStripeConnectedAccount(execProps)
-    },
-    onError({ error, fnProps }) {
-      logError({
-        error,
-        fnName: 'useCreateStripeConnectedAccount',
-        metadata: { props, fnProps },
-      })
-    },
-  })
+          await refetchFirestoreUser()
 
-  return _createConnectedAccount
+          window.location.href = p.result.data?.[0].accountLink.url
+        },
+      },
+    })
+
+  return createStripeConnectedAccount
 }
 
 export type UseCreateConnectedAccountReturn = ReturnType<
