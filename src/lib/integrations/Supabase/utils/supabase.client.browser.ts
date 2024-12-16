7fb6b6c782231f5df1dd7708fbe574a473c36c40
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-15 20:29:27 -0500

    Add validation for Supabase configuration to ensure project URL and public anon key are defined

--- src/lib/integrations/Supabase/utils/supabase.client.browser.ts
+++ src/lib/integrations/Supabase/utils/supabase.client.browser.ts
@@ -2,6 +2,14 @@ import { createClient } from '@supabase/supabase-js'
 
 import supabaseConfig from '../supabase.config.js'
 
+if (!supabaseConfig.projectUrl) {
+  throw new Error('supabaseConfig.projectUrl is undefined')
+}
+
+if (!supabaseConfig.publicAnonKey) {
+  throw new Error('supabaseConfig.publicAnonKey is undefined')
+}
+
 // https://supabase.com/docs/reference/javascript/eq
 const supabaseClientBrowser = createClient(
   supabaseConfig.projectUrl,
