302f485edc6f0ae2418d3fdf9783548ca99ed441
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-18 11:42:52 -0500

    Clear function ignore list in Firebase Functions configuration

--- firebaseFunctions/firebaseFunctions.config.ts
+++ firebaseFunctions/firebaseFunctions.config.ts
@@ -1,12 +1,7 @@
 const firebaseFunctionsConfig = {
   cli: {
     // function names not to be deployed
-    ignoreList: [
-      'updateCreatorPlatformStats',
-      'updateSocialSeedRobloxApi',
-      'cleanSocialSeedRobloxApi',
-      'checkJobsPendingApproval',
-    ],
+    ignoreList: [],
   },
 }
 
