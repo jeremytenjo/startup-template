9f84446e78dfe07a5da459cc0c32494ae4796819
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-20 15:40:32 -0500

    Update user stub ID to 'user1' for consistency in test data

--- src/data/users/users.stubs.ts
+++ src/data/users/users.stubs.ts
@@ -5,7 +5,7 @@ import appConfig from '../../../app.config.js'
 import type UserSchema from './user.schema.js'
 
 const commonProps: UserSchema = {
-  id: '1',
+  id: 'user1',
   displayName: 'user1',
   email: 'user1@email.com',
   profilePhoto: {
