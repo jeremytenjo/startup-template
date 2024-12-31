3027b01c1354b3cd77c758eec7240cacf8fb134e
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-12-30 19:17:19 -0500

    Refactor Firebase function templates to use a unified call function and enhance error handling in hooks

--- devtools/codeGenerator/superCodeGen/templates/firebaseFunctionApiRoutes.ts
+++ devtools/codeGenerator/superCodeGen/templates/firebaseFunctionApiRoutes.ts
@@ -54,16 +54,16 @@ const files: SuperCodeGeneratorFilesSchema = [
       const camelCase = helpers?.changeCase?.camelCase(name)
       const pascalCase = helpers?.changeCase?.pascalCase(name)
 
-      return `import useAsync, { type UseAsyncProps } from '@useweb/use-async'
-      import { httpsCallable } from 'firebase/functions'
+      return `import type { UseAsyncProps } from '@useweb/use-async'
+
       import type {
         ${pascalCase}Props,
         ${pascalCase}Return,
       } from './${name}.js'
-      import { functions } from '../../../src/lib/integrations/Google/Firebase/firebase.js'
 
-      import type {
-        RouteSchemaProps,
+      import {
+         callFirebaseFunction,
+         type RouteSchemaProps,
       } from '@/firebaseFunctions/src/utils/useApiRouteData/useApiRouteData.js'
       
       export type ${pascalCase}ClientProps<RouteSchema extends RouteSchemaProps> = {
@@ -78,23 +78,47 @@ const files: SuperCodeGeneratorFilesSchema = [
       >(
         props: ${pascalCase}ClientProps<RouteSchema>,
       ): Promise<${pascalCase}Return<RouteSchema>> {
-        const ${name} = httpsCallable<
-          ${pascalCase}ClientProps<RouteSchema>['api'],
-          ${pascalCase}Return<RouteSchema>
-        >(functions, '${name}')
-      
-        const res = await ${name}(props.api)
-      
-        return res.data
+        const res = await callFirebaseFunction({
+          name: '${name}',
+          api: props.api,
+        })  
+
+        return res
       }
       
       export type ${pascalCase}ClientReturn<RouteSchema extends RouteSchemaProps> =
         ${pascalCase}Return<RouteSchema>
 
-      export function use${pascalCase}Client<RouteSchema extends RouteSchemaProps>(
+      `
+    },
+  },
+
+  // useAsync hook
+  {
+    path: ({ name, helpers }) => {
+      const pascalCase = helpers?.changeCase?.pascalCase(name)
+      const _name = `use${pascalCase}Client`
+
+      return `utils/${_name}/${_name}.ts`
+    },
+    template: ({ name, helpers }) => {
+      const pascalCase = helpers?.changeCase?.pascalCase(name)
+      const camelCase = helpers?.changeCase?.camelCase(name)
+
+      return `import useAsync from '@useweb/use-async'
+      
+      import type { ApiRouteSchema } from '../../../utils/useApiRouteData/useApiRouteData.js'
+      import logError from '../../../../../src/lib/utils/loggers/logError/logError.js'
+      import type { ${pascalCase}ClientProps } from '../../${camelCase}.client.js'
+      import ${camelCase}Client from '../../${camelCase}.client.js'
+      
+      export function use${pascalCase}Client<RouteSchema extends ApiRouteSchema>(
         props: ${pascalCase}ClientProps<RouteSchema>,
       ) {
-        const ${camelCase} = useAsync<${pascalCase}ClientProps<RouteSchema>['api'], RouteSchema['return']>({
+        const ${camelCase} = useAsync<
+          ${pascalCase}ClientProps<RouteSchema>['api'],
+          RouteSchema['return']
+        >({
           fn: async () => await ${camelCase}Client(props),
           ...props.options,
           onError({ error, fnProps }) {
@@ -108,8 +132,153 @@ const files: SuperCodeGeneratorFilesSchema = [
       
         return ${camelCase}
       }
-      
       `
+    },
+  },
+
+  // useData hook
+  {
+    path: ({ name, helpers }) => {
+      const pascalCase = helpers?.changeCase?.pascalCase(name)
+      const _name = `use${pascalCase}Persist`
+
+      return `utils/${_name}/${_name}.ts`
+    },
+    template: ({ name, helpers }) => {
+      const pascalCase = helpers?.changeCase?.pascalCase(name)
+      const camelCase = helpers?.changeCase?.camelCase(name)
+
+      return `import useData from '@useweb/use-data'
+import logError from '@/src/lib/utils/loggers/logError/logError'
+import { useMemo } from 'react'
+
+import ${camelCase}Client, {
+  type ${pascalCase}ClientProps,
+  type ${pascalCase}ClientReturn,
+} from '../../${camelCase}.client.js'
+import type { ApiRouteSchema } from '../../../utils/useApiRouteData/useApiRouteData.js'
+import type UserSchema from '../../../../../src/data/users/user.schema.js'
+
+export type Use${pascalCase}PersistProps<RouteSchema extends ApiRouteSchema> =
+  ${pascalCase}ClientProps<RouteSchema>
+
+export function get${pascalCase}DataId<RouteSchema extends ApiRouteSchema>(props: {
+  authId: string | undefined
+  route: RouteSchema['route']
+  id: string | undefined
+  allowUnauthenticatedUser: boolean
+}) { 
+  const extraId = props.id ? ${helpers?.wrapInTemplateLiteral({
+    text: `/${helpers?.wrapInTemplateLiteral({
+      text: 'props.id',
+      isPlaceholder: true,
+    })}`,
+  })} : ''
+  let dataId =
+    props.authId && props.route
+      ? ${helpers?.wrapInTemplateLiteral({
+        text: `${camelCase}/${helpers?.wrapInTemplateLiteral({
+          text: 'props.authId',
+          isPlaceholder: true,
+        })}/${helpers?.wrapInTemplateLiteral({
+          text: 'props.route',
+          isPlaceholder: true,
+        })}${helpers?.wrapInTemplateLiteral({
+          text: 'extraId',
+          isPlaceholder: true,
+        })}`,
+      })} 
+      : undefined
+
+  if (props.allowUnauthenticatedUser && props.route) {
+    dataId = ${helpers?.wrapInTemplateLiteral({
+      text: `${camelCase}/${helpers?.wrapInTemplateLiteral({
+        text: 'props.route',
+        isPlaceholder: true,
+      })}${helpers?.wrapInTemplateLiteral({
+        text: 'extraId',
+        isPlaceholder: true,
+      })}`,
+    })}
+  }
+
+  return {
+    id: dataId,
+  }
+}
+
+export default function use${pascalCase}Persist<RouteSchema extends ApiRouteSchema>(
+  props: Partial<Use${pascalCase}PersistProps<RouteSchema>> & {
+    currentUser: UserSchema | undefined
+    id: string | undefined
+    allowUnauthenticatedUser?: boolean
+  },
+) {
+  const id = useMemo(() => {
+    return props?.id
+      ? get${pascalCase}DataId({
+          authId: props.currentUser?.id,
+          route: props.api?.route as string,
+          id: props.id,
+          allowUnauthenticatedUser: Boolean(props.allowUnauthenticatedUser),
+        }).id
+      : undefined
+  }, [props.currentUser?.id, props.api?.route, props.id, props.allowUnauthenticatedUser])
+
+  const ${camelCase} = useData<
+    Awaited<${pascalCase}ClientReturn<RouteSchema>['data']>,
+    ${pascalCase}ClientProps<RouteSchema>,
+    ${pascalCase}ClientProps<RouteSchema>['api']['payload']
+  >({
+    id,
+    get: {
+      fetcher: async (p) => {
+        const ${camelCase}Res = await ${camelCase}Client<RouteSchema>({
+          options: props.options,
+          api: {
+            route: props.api?.route,
+            payload: {
+              ...(props.api?.payload || {}),
+              ...(p.api?.payload || {}),
+            },
+          },
+        })
+
+        if (${camelCase}Res.data) {
+          return ${camelCase}Res.data
+        }
+
+        return []
+      },
+      onGet({ result }) {
+        if (props.options?.onResult) {
+          props.options.onResult({
+            result: {
+              data: [result[0]['0']],
+            },
+          })
+        }
+      },
+      onGetError({ error }) {
+        logError({
+          error,
+          fnName: ${helpers?.wrapInTemplateLiteral({
+            text: `use${pascalCase}Persist - get - ${helpers?.wrapInTemplateLiteral({
+              text: 'props.api?.route',
+              isPlaceholder: true,
+            })}`,
+          })} ,
+          metadata: { props },
+        })
+      },
+    },
+  })
+
+  return ${camelCase}
+}
+
+export type Use${pascalCase}Return = ReturnType<typeof use${pascalCase}Persist>
+`
     },
   },
 
