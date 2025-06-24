e12e1c0b0f120b703ff337ca5586d14fa09479cf
jeremytenjo <tenjojeremy@gmail.com>
2025-06-24 13:53:27 -0400

    refactor: remove RootLayout wrapper from NotFound component for cleaner structure

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,6 @@ import Link from '@useweb/ui/Link'
 import EmptyMessage from '@useweb/ui/EmptyMessage'
 import Box from '@useweb/ui/Box'
 
-import RootLayout from '../lib/layouts/RootLayout/RootLayout.js'
 import PageNotFoundIcon from '../lib/components/icons/PageNotFoundIcon.js'
 
 export const metadata = {
@@ -13,54 +12,52 @@ export const metadata = {
 
 export default function NotFound() {
   return (
-    <RootLayout>
-      <Box
+    <Box
+      sx={{
+        '& img': {
+          width: '100%',
+          height: '100%',
+          maxWidth: '470px',
+        },
+      }}
+    >
+      <EmptyMessage
+        title='Page not found'
+        subTitle="Sorry, the page you're looking for can't be found"
+        icon={
+          <PageNotFoundIcon
+            sx={{
+              fontSize: '200px',
+            }}
+          />
+        }
         sx={{
-          '& img': {
-            width: '100%',
-            height: '100%',
-            maxWidth: '470px',
-          },
+          display: 'block',
+          m: '0 auto',
+          my: '40px',
+          maxWidth: '100%',
         }}
-      >
-        <EmptyMessage
-          title='Page not found'
-          subTitle="Sorry, the page you're looking for can't be found"
-          icon={
-            <PageNotFoundIcon
+        content={
+          <Link
+            href={'/'}
+            sx={{
+              mt: 2,
+              display: 'block',
+            }}
+          >
+            <Button
+              name='go back home'
               sx={{
-                fontSize: '200px',
-              }}
-            />
-          }
-          sx={{
-            display: 'block',
-            m: '0 auto',
-            my: '40px',
-            maxWidth: '100%',
-          }}
-          content={
-            <Link
-              href={'/'}
-              sx={{
-                mt: 2,
+                width: 'fit-content',
                 display: 'block',
+                m: '0 auto',
               }}
             >
-              <Button
-                name='go back home'
-                sx={{
-                  width: 'fit-content',
-                  display: 'block',
-                  m: '0 auto',
-                }}
-              >
-                Go back home
-              </Button>
-            </Link>
-          }
-        />
-      </Box>
-    </RootLayout>
+              Go back home
+            </Button>
+          </Link>
+        }
+      />
+    </Box>
   )
 }
