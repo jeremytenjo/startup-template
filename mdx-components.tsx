9f5bb52cf478824ada060cc165d2c7e955aa3b0a
jeremytenjo <tenjojeremy@gmail.com>
2025-07-25 11:55:53 -0400

    feat: update privacy policy with dynamic date and site information; enhance MDX components with custom headings and paragraph styles

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,4 +1,9 @@
+import React from 'react'
+import Text from '@useweb/ui/Text'
 import type { MDXComponents } from 'mdx/types'
+
+import PageTitleHeading from './lib/layouts/PageTitleHeading/PageTitleHeading.js'
+import { variants } from './lib/integrations/Useweb/theme/tokens/typography/typography.js'
 
 /**
  * Customize MDX components for Next.js MDX integration
@@ -8,6 +13,95 @@ import type { MDXComponents } from 'mdx/types'
  */
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: ({ children }) => {
+      return <PageTitleHeading title={String(children)} />
+    },
+
+    h2: ({ children }) => {
+      return (
+        <Text
+          text={String(children)}
+          tag='h2'
+          sx={{
+            ...variants.h6,
+            mb: 1,
+          }}
+        />
+      )
+    },
+
+    h3: ({ children }) => {
+      return (
+        <Text
+          text={String(children)}
+          tag='h3'
+          sx={{
+            ...variants.h6,
+            mb: 1,
+          }}
+        />
+      )
+    },
+
+    h4: ({ children }) => {
+      return (
+        <Text
+          text={String(children)}
+          tag='h4'
+          sx={{
+            ...variants.h6,
+            mb: 1,
+          }}
+        />
+      )
+    },
+
+    h5: ({ children }) => {
+      return (
+        <Text
+          text={String(children)}
+          tag='h5'
+          sx={{
+            ...variants.h6,
+            mb: 1,
+          }}
+        />
+      )
+    },
+
+    h6: ({ children }) => {
+      return (
+        <Text
+          text={String(children)}
+          tag='h6'
+          sx={{
+            ...variants.h6,
+            mb: 1,
+          }}
+        />
+      )
+    },
+
+    p: ({ children }) => {
+      return (
+        <Text
+          text={String(children)}
+          tag='p'
+          sx={{
+            mb: 2,
+          }}
+        />
+      )
+    },
+
+    li: ({ children }) => {
+      return (
+        <li style={{ marginBottom: '0.5rem' }}>
+          <Text text={children} tag='p' sx={{}} />
+        </li>
+      )
+    },
+
     ...components,
   }
 }
