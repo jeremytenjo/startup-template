09734226d955309ec6efd4bd7eb7462bcd2bc5d9
jeremytenjo <tenjojeremy@gmail.com>
2025-08-12 08:23:02 -0400

    fix: standardize formatting and correct event name casing in phError and nodePhError functions

--- lib/integrations/PostHog/events/browser/ph_error/phError.ts
+++ lib/integrations/PostHog/events/browser/ph_error/phError.ts
@@ -1,26 +1,26 @@
-import assert from '@useweb/assert'
+import assert from '@useweb/assert';
 
-import postHog from '../../../utils/postHog.js'
+import postHog from '../../../utils/postHog.js';
 
 export type PhErrorProps = {
-  fnName: string
-  description: string
-  fatal?: boolean
-  metadata?: any
-}
+  fnName: string;
+  description: string;
+  fatal?: boolean;
+  metadata?: any;
+};
 
 export default async function phError(props: PhErrorProps) {
-  assert<PhErrorProps>({ props, requiredProps: ['description', 'fnName'] })
+  assert<PhErrorProps>({ props, requiredProps: ['description', 'fnName'] });
 
   postHog({
-    eventName: 'Error',
+    eventName: 'error',
     data: {
       description: String(props.description),
       functionName: props.fnName,
       fatal: Boolean(props.fatal),
       metadata: props.metadata,
     },
-  })
+  });
 }
 
-export type PhErrorReturn = ReturnType<typeof phError>
+export type PhErrorReturn = ReturnType<typeof phError>;
